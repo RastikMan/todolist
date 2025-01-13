9942ef4ea418ccc5ac08eb9d6d8cdb772b449c9d
rmergeee <owodow.rostislaw@gmail.com>
2025-01-13 18:41:04 +0200

    server running

--- main.go
+++ main.go
@@ -9,4 +9,7 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Welcome to the To-Do List API")
 	})
+
+	fmt.Println("Server is running on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
 }
